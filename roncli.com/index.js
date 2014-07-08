33c71d2b350eff2c7982b9ff4d4c0585823424d3
Ronald M. Clifford <roncli@roncli.com>
2014-07-08 12:56:31 -0500

    Better interceptor.js test.

--- roncli.com/index.js
+++ roncli.com/index.js
@@ -75,7 +75,7 @@ function start() {
 }
 
 // Only start server if this script is executed, and not called via require.
-if (require.main && (require.main === module || (require.main.filename.match(/interceptor\.js$/) && require.main.children[0] === module))) {
+if (require.main && (require.main === module || (/interceptor\.js$/.test(require.main.filename) && require.main.children[0] === module))) {
     start();
 } else {
     console.error("You can only load this website if it is the main script that is executed, or if it is loaded through iisnode.  If iisnode is installed in a non-standard location, you will need to modify the location in the index.js file.");
