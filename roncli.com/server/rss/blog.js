8e266be39aeb6858143484398766f3aec15445a2
Ronald M. Clifford <roncli@roncli.com>
2015-01-28 01:04:55 -0600

    Further work on blog RSS.

--- roncli.com/server/rss/blog.js
+++ roncli.com/server/rss/blog.js
@@ -1,7 +1,45 @@
-var rss = require("rss");
+var cache = require("../cache/cache"),
+    Rss = require("rss"),
+    blog = require("../models/blog");
 
+/**
+ * Get the RSS feed for the blog.
+ * @param {object} req The Express request object.
+ * @param {object} res The Express response object.
+ * @param {function(null, object)|function(object)} callback The callback function.
+ */
 module.exports.rss = function(req, res, callback) {
     "use strict";
 
-    var startIndex = req.query["start-index"] || 1;
+    var startIndex = req.query["start-index"] || 1,
+        category = req.query["category"],
+        cacheKey = "roncli.com:rss:blog:" + startIndex + (category ? ":" + category : ""),
+        feed = new Rss();
+
+    cache.get(cacheKey, function(rss) {
+        if (rss) {
+            callback(null, rss);
+            return;
+        }
+
+        feed.title = "roncli.com Blog";
+        feed.description = "The blog of roncli";
+        feed.generator = "roncli.com";
+        feed.feed_url = "http://roncli.com/blog.rss";
+        feed.site_url = "http://roncli.com/blog";
+        feed.image_url = "http://roncli.com/images/roncliSmall.png";
+        feed.managingEditor = "roncli@roncli.com (Ronald M. Clifford)";
+        feed.webMaster = "roncli@roncli.com (Ronald M. Clifford)";
+        feed.copyright = (new Date()).getFullYear().toString() + " Ronald M. Clifford";
+        feed.language = "en-us";
+        // feed.categories
+        feed.ttl = 60;
+        feed.custom_namespaces.openSearch = "http://a9.com/-/spec/opensearchrss/1.0/";
+        feed.custom_namespaces.thr = "http://purl.org/syndication/thread/1.0";
+        // feed.custom_elements.push({"openSearch:totalResults": ""});
+        feed.custom_elements.push({"openSearch:startIndex": startIndex.toString()});
+        feed.custom_elements.push({"openSearch:itemsPerPage": "25"});
+    });
+
+
 };
