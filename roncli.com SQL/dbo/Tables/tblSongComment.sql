7a5c772d585910ddd997033337b0270d0581c296
Ronald M. Clifford <roncli@roncli.com>
2015-02-22 17:56:18 -0600

    Update database for music section.

--- roncli.com SQL/dbo/Tables/tblSongComment.sql
+++ roncli.com SQL/dbo/Tables/tblSongComment.sql
@@ -1,12 +1,11 @@
 ﻿CREATE TABLE [dbo].[tblSongComment] (
     [CommentID]     INT      IDENTITY (1, 1) NOT NULL,
-    [SongID]        INT      NOT NULL,
-    [UserID]        INT      NOT NULL,
+    [SongURL]       VARCHAR(1024)      NOT NULL,
     [Comment]       TEXT     NOT NULL,
     [CrDate]        DATETIME CONSTRAINT [DF_tblSongComment_CrDate] DEFAULT (getutcdate()) NOT NULL,
+    [CrUserID]      INT      NOT NULL,
     [ModeratedDate] DATETIME NULL,
     CONSTRAINT [PK_tblSongComment] PRIMARY KEY CLUSTERED ([CommentID] ASC),
-    CONSTRAINT [FK_tblSongComment_tblSong] FOREIGN KEY ([SongID]) REFERENCES [dbo].[tblSong] ([SongID]),
-    CONSTRAINT [FK_tblSongComment_tblUser] FOREIGN KEY ([UserID]) REFERENCES [dbo].[tblUser] ([UserID])
+    CONSTRAINT [FK_tblSongComment_tblUser] FOREIGN KEY ([CrUserID]) REFERENCES [dbo].[tblUser] ([UserID])
 );
 
