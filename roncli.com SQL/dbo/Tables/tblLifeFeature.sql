f06c05daef85e899d5bb44c68be80ab9734c1e38
Ronald M. Clifford <roncli@roncli.com>
2015-02-28 15:49:20 -0600

    Update VIsual Studio project and remove some unneeded database tables.

--- roncli.com SQL/dbo/Tables/tblLifeFeature.sql
+++ roncli.com SQL/dbo/Tables/tblLifeFeature.sql
@@ -2,7 +2,7 @@
     [FeatureID] INT      IDENTITY (1, 1) NOT NULL,
     [PageID]    INT      NOT NULL,
     [Order]     INT      NOT NULL,
-    [CrDate]    DATETIME NOT NULL,
+    [CrDate]    DATETIME CONSTRAINT [DF_tblLifeFeature_CrDate] DEFAULT (getutcdate()) NOT NULL,
     CONSTRAINT [PK_tblLifeFeature] PRIMARY KEY CLUSTERED ([FeatureID] ASC),
     CONSTRAINT [FK_tblLifeFeature_tblPage] FOREIGN KEY ([PageID]) REFERENCES [dbo].[tblPage] ([PageID])
 );
--- roncli.com SQL/dbo/Tables/tblProject.sql
+++ roncli.com SQL/dbo/Tables/tblProject.sql
@@ -1,13 +1,12 @@
 ﻿CREATE TABLE [dbo].[tblProject] (
     [ProjectID]   INT           IDENTITY (1, 1) NOT NULL,
     [Title]       VARCHAR (255) NOT NULL,
-    [URL]         VARCHAR (255) NULL,
+    [URL]         VARCHAR (1024) NULL,
+    [User]        VARCHAR (50)  NULL,
+    [Repository]  VARCHAR (50)  NULL,
     [Description] TEXT          NOT NULL,
-    [LicenseID]   INT           NOT NULL,
-    [Public]      BIT           NOT NULL,
-    [CrDate]      DATETIME      NOT NULL,
+    [CrDate]      DATETIME      CONSTRAINT [DF_tblProject_CrDate] DEFAULT (getutcdate()) NOT NULL,
     [UpdDate]     DATETIME      CONSTRAINT [DF_tblProject_UpdDate] DEFAULT (getutcdate()) NOT NULL,
-    CONSTRAINT [PK_tblProject] PRIMARY KEY CLUSTERED ([ProjectID] ASC),
-    CONSTRAINT [FK_tblProject_tblLicense] FOREIGN KEY ([LicenseID]) REFERENCES [dbo].[tblLicense] ([LicenseID])
+    CONSTRAINT [PK_tblProject] PRIMARY KEY CLUSTERED ([ProjectID] ASC)
 );
 
