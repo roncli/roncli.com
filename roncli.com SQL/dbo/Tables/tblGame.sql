314ec6273c537739eee07fdad1e5366df286d91e
Ronald M. Clifford <roncli@roncli.com>
2015-03-24 16:43:35 -0500

    Update SQL database objects to remove tables not in use.

--- roncli.com SQL/dbo/Tables/tblGame.sql
+++ roncli.com SQL/dbo/Tables/tblGame.sql
@@ -2,7 +2,7 @@
     [GameID]       INT           IDENTITY (1, 1) NOT NULL,
     [Title]        VARCHAR (255) NOT NULL,
     [Description]  TEXT          NOT NULL,
-    [IconFilename] VARCHAR (255) NOT NULL,
+    [IconURL] VARCHAR (1024) NOT NULL,
     [CrDate]       DATETIME      CONSTRAINT [DF_tblGame_CrDate] DEFAULT (getutcdate()) NOT NULL,
     [UpdDate]      DATETIME      CONSTRAINT [DF_tblGame_UpdDate] DEFAULT (getutcdate()) NOT NULL,
     CONSTRAINT [PK_tblGame] PRIMARY KEY CLUSTERED ([GameID] ASC)
