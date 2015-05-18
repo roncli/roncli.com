e11d5bcd096d70435e92c796168343c759ea4e1d
Ronald M. Clifford <roncli@roncli.com>
2015-05-18 03:05:47 -0500

    Load twitch.tv on the home page if the stream is live.

--- roncli.com/app/views/home/index.js
+++ roncli.com/app/views/home/index.js
@@ -1,3 +1,5 @@
+/*global swfobject*/
+
 var BaseView = require("rendr/shared/base/view"),
     $ = require("jquery");
 
@@ -8,16 +10,41 @@ module.exports = BaseView.extend({
     postRender: function() {
         "use strict";
 
-        $("div.blog img").each(function() {
-            var image = $(this);
-            image.load(function() {
-                var width = this.width;
-                $("<img />").attr("src", $(this).attr("src")).load(function() {
-                    if (width !== this.width) {
-                        image.addClass("thumb").attr("title", "Click to view full image in a new window");
-                    }
-                });
+        var view = this,
+            app = this.app,
+            panel = $("#twitch-panel");
+
+        // Load Twitch stream
+        window.onPlayerEvent = function(data) {
+            if (app.router.currentView !== view) {
+                app.twitchPlayer = null;
+                window.onPlayerEvent = null;
+                return;
+            }
+            data.forEach(function(event) {
+                switch (event.event) {
+                    case "playerInit":
+                        app.twitchPlayer = $("#twitch-stream")[0];
+                        app.twitchPlayer.playVideo();
+                        break;
+                    case "online":
+                        panel.attr("style", "");
+                        break;
+                    case "offline":
+                        panel.attr("style", "height: 0; margin-bottom: 0;");
+                        break;
+                }
             });
+        };
+
+        swfobject.embedSWF("//www-cdn.jtvnw.net/swflibs/TwitchPlayer.swf", "twitch-stream", "100%", "400", "11", null, {
+            "eventsCallback": "onPlayerEvent",
+            "embed": 1,
+            "channel": "roncli",
+            "auto_play": "true"
+        }, {
+            "allowScriptAccess": "always",
+            "allowFullScreen": "true"
         });
     }
 });
