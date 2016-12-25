6c082f9160e62c166f21d85ff7c170e3cf2249c3
Ronald M. Clifford <roncli@roncli.com>
2016-12-25 00:01:38 -0800

    Update Visual Studio projects.

--- roncli.com SQL/dbo/Tables/tblDCLPlaylist.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[tblDCLPlaylist] (
-    [PlaylistID] VARCHAR(64) NOT NULL,
-    [SeasonEndDate] DATETIME NOT NULL, 
-    CONSTRAINT [PK_tblDCLPlaylist] PRIMARY KEY CLUSTERED ([PlaylistID] ASC),
-);
-
