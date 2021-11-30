c2e69c151ace893ab169f131f23149056a7fde90
Ronald M. Clifford <roncli@roncli.com>
2021-11-29 23:27:17 -0800

    Photoprism updates.

--- photoprism/start.sh
+++ photoprism/start.sh
@@ -2,8 +2,9 @@
 
 if [ $USE_AZURE_FILE_STORAGE -eq 1 ];
 then
-    # Mount Azure Storage file share.
-    mount -t cifs $(cat $LIBRARY_URI) /ps/library -o vers=3.0,username=$(cat $LIBRARY_USERNAME),password=$(cat $LIBRARY_PASSWORD),dir_mode=0755,file_mode=0755,serverino
+    # Mount Azure Storage file shares.
+    mount -t cifs $(cat $LIBRARY_URI) /photoprism/originals -o vers=3.0,username=$(cat $LIBRARY_USERNAME),password=$(cat $LIBRARY_PASSWORD),dir_mode=0755,file_mode=0755,serverino
+    mount -t cifs $(cat $STORAGE_URI) /photoprism/storage -o vers=3.0,username=$(cat $STORAGE_USERNAME),password=$(cat $STORAGE_PASSWORD),dir_mode=0755,file_mode=0755,serverino
 fi
 
 # Run app.
