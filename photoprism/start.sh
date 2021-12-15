70a0b3a313d8200c55a453ba45198f48f6e5abd2
Ronald M. Clifford <roncli@roncli.com>
2021-12-14 16:12:38 -0800

    Use Azure storage from host.

--- photoprism/start.sh
+++ photoprism/start.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-if [ $USE_AZURE_FILE_STORAGE -eq 1 ];
-then
-    # Mount Azure Storage file shares.
-    mount -t cifs $(cat $LIBRARY_URI) /photoprism/originals -o vers=3.0,username=$(cat $LIBRARY_USERNAME),password=$(cat $LIBRARY_PASSWORD),dir_mode=0755,file_mode=0755,serverino
-    mount -t cifs $(cat $STORAGE_URI) /photoprism/storage -o vers=3.0,username=$(cat $STORAGE_USERNAME),password=$(cat $STORAGE_PASSWORD),dir_mode=0755,file_mode=0755,serverino
-fi
-
 # Run app.
 PHOTOPRISM_LOG_LEVEL=error PHOTOPRISM_PUBLIC=true PHOTOPRISM_SITE_URL=https://photos.roncli.com/ PHOTOPRISM_DOWNLOAD_TOKEN=public PHOTOPRISM_SITE_AUTHOR="Ronald M. Clifford" PHOTOPRISM_SITE_TITLE="roncli.com Photos" PHOTOPRISM_SITE_CAPTION="Music. Coding. Gaming. Life." /entrypoint.sh photoprism start
