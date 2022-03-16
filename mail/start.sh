6e1e5c0e3833ca24fb4bfa4b66a4f0418964fd5d
Ronald M. Clifford <roncli@roncli.com>
2022-03-15 22:23:41 -0700

    Fix environment variable name.

--- mail/start.sh
+++ mail/start.sh
@@ -11,4 +11,4 @@ then
     ln -s /etc/letsencrypt/live/$DOMAIN/privkey.pem /etc/postfix/cert/smtp.key
 fi
 
-SMF_RELAYHOSTAUTH="$SMTP_USERNAME:$(cat $SMTP_PASSWORD_FILE)" /entrypoint.sh
+SMF_RELAYAUTH="$SMTP_USERNAME:$(cat $SMTP_PASSWORD_FILE)" /entrypoint.sh
