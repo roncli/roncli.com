2f78d335fe4eae5438b19772c752f42fcc5df460
Ronald M. Clifford <roncli@roncli.com>
2022-08-12 22:06:02 -0700

    Refactor of Twitter post retrieval.

--- node-roncli-com/src/twitter/index.js
+++ node-roncli-com/src/twitter/index.js
@@ -1,7 +1,14 @@
+/**
+ * @typedef {{id: string, tweet: TwitterApi.TweetV2, author: TwitterApi.UserV2}} Tweet
+ */
+
 const Discord = require("../discord"),
     Log = require("@roncli/node-application-insights-logger"),
     TwitterModel = require("../models/twitter"),
-    TwitterApi = require("twitter-api-v2").TwitterApi;
+    TwitterApi = require("twitter-api-v2");
+
+/** @type {TwitterApi.TwitterApiReadOnly} */
+let client = void 0;
 
 //  #####           #     #      #
 //    #                   #      #
@@ -30,46 +37,15 @@ class Twitter {
         }
 
         try {
+            // Get the client.
+            client = await Twitter.getClient();
+
             // Get the last Tweet.
             const twitter = await TwitterModel.get(),
                 lastId = twitter.lastId;
 
-            const client = new TwitterApi({
-                appKey: process.env.TWITTER_API_KEY,
-                appSecret: process.env.TWITTER_API_SECRET,
-                accessToken: process.env.TWITTER_ACCESS_TOKEN,
-                accessSecret: process.env.TWITTER_ACCESS_SECRET
-            });
-
-            const appClient = (await client.appLogin()).readOnly,
-                tweets = [];
-
-            const timeline = await appClient.v2.userTimeline(process.env.TWITTER_ID, {
-                "since_id": lastId,
-                "max_results": 100,
-                exclude: "replies",
-                expansions: ["author_id", "referenced_tweets.id.author_id"],
-                "tweet.fields": ["in_reply_to_user_id", "created_at", "referenced_tweets"],
-                "user.fields": ["name", "profile_image_url"]
-            });
-
-            for await (const tweet of timeline) {
-                if (tweet.referenced_tweets && tweet.referenced_tweets[0].type === "retweeted") {
-                    const retweet = timeline.includes.tweetById(tweet.referenced_tweets[0].id);
-
-                    tweets.push({
-                        id: tweet.id,
-                        tweet: retweet,
-                        author: timeline.includes.userById(retweet.author_id)
-                    });
-                } else {
-                    tweets.push({
-                        id: tweet.id,
-                        tweet,
-                        author: timeline.includes.userById(tweet.author_id)
-                    });
-                }
-            }
+            // Check the timeline for new posts.
+            const tweets = await Twitter.getNewPosts(lastId);
 
             if (tweets.length > 0) {
                 tweets.sort((a, b) => a.tweet.id.localeCompare(b.tweet.id));
@@ -108,6 +84,74 @@ class Twitter {
         }
 
         setTimeout(Twitter.checkPosts, 300000);
+    }
+
+    //              #     ##   ##     #                 #
+    //              #    #  #   #                       #
+    //  ###   ##   ###   #      #    ##     ##   ###   ###
+    // #  #  # ##   #    #      #     #    # ##  #  #   #
+    //  ##   ##     #    #  #   #     #    ##    #  #   #
+    // #      ##     ##   ##   ###   ###    ##   #  #    ##
+    //  ###
+    /**
+     * Gets a logged in read-only Twitter client.
+     * @returns {Promise<TwitterApi.TwitterApiReadOnly>} A read-only Twitter client.
+     */
+    static async getClient() {
+        const api = new TwitterApi.TwitterApi({
+            appKey: process.env.TWITTER_API_KEY,
+            appSecret: process.env.TWITTER_API_SECRET,
+            accessToken: process.env.TWITTER_ACCESS_TOKEN,
+            accessSecret: process.env.TWITTER_ACCESS_SECRET
+        });
+
+        return (await api.appLogin()).readOnly;
+    }
+
+    //              #    #  #              ###                 #
+    //              #    ## #              #  #                #
+    //  ###   ##   ###   ## #   ##   #  #  #  #   ##    ###   ###    ###
+    // #  #  # ##   #    # ##  # ##  #  #  ###   #  #  ##      #    ##
+    //  ##   ##     #    # ##  ##    ####  #     #  #    ##    #      ##
+    // #      ##     ##  #  #   ##   ####  #      ##   ###      ##  ###
+    //  ###
+    /**
+     * Gets the newest Twitter posts based on the last received ID.
+     * @param {string} lastId The last tweet ID.
+     * @returns {Promise<Tweet[]>} A list of Twitter posts.
+     */
+    static async getNewPosts(lastId) {
+        /** @type {Tweet[]} */
+        const tweets = [];
+
+        const timeline = await client.v2.userTimeline(process.env.TWITTER_ID, {
+            "since_id": lastId,
+            "max_results": 100,
+            exclude: "replies",
+            expansions: ["author_id", "referenced_tweets.id.author_id"],
+            "tweet.fields": ["in_reply_to_user_id", "created_at", "referenced_tweets"],
+            "user.fields": ["name", "profile_image_url"]
+        });
+
+        for await (const tweet of timeline) {
+            if (tweet.referenced_tweets && tweet.referenced_tweets[0].type === "retweeted") {
+                const retweet = timeline.includes.tweetById(tweet.referenced_tweets[0].id);
+
+                tweets.push({
+                    id: tweet.id,
+                    tweet: retweet,
+                    author: timeline.includes.userById(retweet.author_id)
+                });
+            } else {
+                tweets.push({
+                    id: tweet.id,
+                    tweet,
+                    author: timeline.includes.userById(tweet.author_id)
+                });
+            }
+        }
+
+        return tweets;
     }
 }
 
